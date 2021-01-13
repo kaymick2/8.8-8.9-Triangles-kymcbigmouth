6b264e046633f77c5c97798caf7ae1f8e042fb35
Kyle McDowell <kymcd21@icstudents.org>
2021-01-13 18:48:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    double sideA=0;
+    double sideB=0;
+    double sideC=0;
+    Triangleops userTriangle=new Triangleops(sideA, sideB, sideC);
+    
+    
+  }
+}
---
+++ Triangleops.java
@@ -0,0 +1,14 @@
+public class Triangleops{
+  private double sideA;
+  private double sideB;
+  private double sideC;
+  public Triangleops (double sideA, double sideB, double sideC){
+    this.sideA=sideA;
+    this.sideB=sideB;
+    this.sideC=sideC;
+  }
+  public test (double sideA, double sideB, double sideC){
+    
+  }
+  
+  }
